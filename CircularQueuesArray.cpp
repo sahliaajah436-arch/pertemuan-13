c306e80f80dd3999b4e0a65b6472ee2335ac88cb
sahliaajah436-arch <sahliaajah436@gmail.com>
2026-05-28 11:49:45 +0700

    menambahkan variabel front dan rear

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -4,3 +4,15 @@ using namespace std;
 class Queues
 {
 private:
+
+    static const int max = 3;
+    int FRONT, REAR;
+    int queue_array[max];
+
+public:
+
+    Queues()
+    {
+        FRONT = -1;
+        REAR = -1;
+    }
